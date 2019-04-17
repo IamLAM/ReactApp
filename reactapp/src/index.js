4bf80736cff5fe37120c3120dc575191ff500418
IamLAM <ingeniero@luismonge.com.mx>
2019-04-17 01:01:52 -0700

    override a default props

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -201,3 +201,23 @@ const CurrentDate = (props) => {
   ReactDOM.render(<ToDo/>,document.getElementById('root'));
 
   //if you want to asign a default props ShoppingCart.defaultProps = {items: 0}
+
+  //override a default props
+  const Items = (props) => {
+    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+  }
+  
+  Items.defaultProps = {
+    quantity: 0
+  }
+  
+  class ShoppingCart extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      { /* change code below this line */ }
+      return <Items quantity={10} />
+      { /* change code above this line */ }
+    }
+  };
