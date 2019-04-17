3717471ea9d6cd2334d03dd77b3a7d93a50ed67d
IamLAM <ingeniero@luismonge.com.mx>
2019-04-17 00:42:07 -0700

    Using props

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -143,3 +143,31 @@ const TypesOfFruit = () => {
     }
     
     ReactDOM.render(<MyComponent/>,document.getElementById("challenge-node"));
+
+    //good example using props
+
+const CurrentDate = (props) => {
+    return (
+      <div>
+        { /* change code below this line */ }
+        <p>The current date is:{props.date} </p>
+        { /* change code above this line */ }
+      </div>
+    );
+  };
+  
+  class Calendar extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h3>What date is it?</h3>
+          { /* change code below this line */ }
+          <CurrentDate date={Date()}/>
+          { /* change code above this line */ }
+        </div>
+      );
+    }
+  };
