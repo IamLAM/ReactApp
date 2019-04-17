d671d4b9aadc65e95d9cb25eb525eebb4039af74
IamLAM <ingeniero@luismonge.com.mx>
2019-04-17 00:44:33 -0700

    Passing array by props

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -171,3 +171,29 @@ const CurrentDate = (props) => {
       );
     }
   };
+
+  //passing array by props
+  const List= (props) => {
+    { /* change code below this line */ }
+    return <p>{}</p>
+    { /* change code above this line */ }
+  };
+  
+  class ToDo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>To Do Lists</h1>
+          <h2>Today</h2>
+          { /* change code below this line */ }
+          <List tasks={[""]}/>
+          <h2>Tomorrow</h2>
+          <List/>
+          { /* change code above this line */ }
+        </div>
+      );
+    }
+  };
