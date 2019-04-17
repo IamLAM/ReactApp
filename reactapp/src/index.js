4d62d86274f2b57d17fd38243878116e8447faf4
IamLAM <ingeniero@luismonge.com.mx>
2019-04-16 22:15:49 -0700

    if you need to use break or hr , you must to use this

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 const JSX=(<div>
 <h1>Valid JSX</h1>
 <p>Unordered list</p>
+<hr></hr>
+<br/> 
 <ul>
 <li>hi</li>
 <li>hi</li>
@@ -14,3 +16,4 @@ const JSX=(<div>
 ReactDOM.render(JSX, document.getElementById('root'))
 //first parameters is component and second is a section of html
 //if we need to add a class , we need to put classNames
+
