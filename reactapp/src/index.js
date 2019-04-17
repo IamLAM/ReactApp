2db80d139bdc72691f9864f4093efd1121652187
IamLAM <ingeniero@luismonge.com.mx>
2019-04-17 00:29:27 -0700

    Basic component react

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -122,3 +122,24 @@ const TypesOfFruit = () => {
       );
     }
   };
+
+
+  //classic and basic component
+  class MyComponent extends React.Component{
+
+    constructor(props){
+    super(props);
+    
+    }
+    
+    render(){
+      return(
+    
+        <div><h1>My First React Component</h1></div>
+    
+      );
+    }
+    
+    }
+    
+    ReactDOM.render(<MyComponent/>,document.getElementById("challenge-node"));
