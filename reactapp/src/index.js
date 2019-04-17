ea254af12736f25015c50493ccd0d9e080a9e020
IamLAM <ingeniero@luismonge.com.mx>
2019-04-16 19:35:20 -0700

    comments

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,6 +1,8 @@
+/*introduction to react */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-const JSX=<div>
+const JSX=(<div>
 <h1>Valid JSX</h1>
 <p>Unordered list</p>
 <ul>
@@ -8,5 +10,5 @@ const JSX=<div>
 <li>hi</li>
 <li>hi</li>
 </ul>
-</div>;
+</div>);
 ReactDOM.render(JSX, document.getElementById('root'))
