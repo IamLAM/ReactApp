43904d42987ee997f5f2f3097e3a285c2cb04b7e
IamLAM <ingeniero@luismonge.com.mx>
2019-04-19 03:07:18 -0700

    if you need to update a state, you need to use setState

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -308,7 +308,8 @@ class MyComponent extends React.Component {
   }
 };
 
-//another way to get and render state
+//
+
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -325,6 +326,33 @@ class MyComponent extends React.Component {
         { /* change code below this line */ }
       <h1>{name}</h1>
         { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+//if you need to update a state, you need to use setState
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // change code below this line
+this.setState({
+  name: 'React Rocks!'
+});
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
       </div>
     );
   }
