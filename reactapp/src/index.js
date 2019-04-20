f763f9522c534926b475601ab3134ea30225b04a
IamLAM <ingeniero@luismonge.com.mx>
2019-04-20 14:46:25 -0700

    Input controlled

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -444,4 +444,34 @@ class Counter extends React.Component {
   }
 };
 
-ReactDOM.render(<Counter/>,document.getElementById("root"));
+//ReactDOM.render(<Counter/>,document.getElementById("root"));
+
+//handle a input form
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // change code below this line
+
+    // change code above this line
+  }
+  // change code below this line
+handleChange(event){
+
+
+}
+  // change code above this line
+  render() {
+    return (
+      <div>
+        { /* change code below this line */}
+
+        { /* change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
