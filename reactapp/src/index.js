802035472c33b48e70f14e214b72b13758a1ae28
IamLAM <ingeniero@luismonge.com.mx>
2019-04-21 00:04:49 -0700

    Using &&

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -890,3 +890,31 @@ class MyComponent9 extends React.Component {
   }
   }
 };
+
+
+
+//
+
+class MyComponent10 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState({
+      display: !this.state.display
+    });
+  }
+  render() {
+    // change code below this line
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         <h1>Displayed!</h1>
+       </div>
+    );
+  }
+};
