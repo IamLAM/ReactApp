b2db8c5fdc57f4b5d2d22d915087f674170f5598
IamLAM <ingeniero@luismonge.com.mx>
2019-04-20 22:35:22 -0700

    using a lifecycle method componentDidMount

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -594,4 +594,28 @@ class MyComponent6 extends React.Component {
   }
 };
 
-ReactDOM.render(<MyComponent6/>,document.getElementById("root"));
+//ReactDOM.render(<MyComponent6/>,document.getElementById("root"));
+
+//using a lifecycle method componentDidMount
+class MyComponent7 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: { /* change code here */ }</h1>
+      </div>
+    );
+  }
+};
