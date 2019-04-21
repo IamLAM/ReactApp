3a2061ad9b36bf85c046dd698fcb42e35bfc31d7
IamLAM <ingeniero@luismonge.com.mx>
2019-04-20 23:55:58 -0700

    some vars included

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -830,7 +830,7 @@ class MagicEightBall extends React.Component {
       'Outlook not so good',
       'Very doubtful'
     ];
-    const answer = 'change me!' // << change code here
+    const answer = possibleAnswers[this.state.randomIndex];
     return (
       <div>
         <input
@@ -844,7 +844,7 @@ class MagicEightBall extends React.Component {
         <h3>Answer:</h3>
         <p>
           { /* change code below this line */ }
-
+            {answer}
           { /* change code above this line */ }
         </p>
       </div>
