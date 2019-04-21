ea94ac34abb610563715d70173a1cbb715c0b255
IamLAM <ingeniero@luismonge.com.mx>
2019-04-20 23:38:38 -0700

    Add Inline Styles in React

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -762,3 +762,16 @@ class Colorful extends React.Component {
     );
   }
 };
+
+//Add Inline Styles in React
+const styles =
+// change code above this line
+class Colorful extends React.Component {
+  render() {
+    // change code below this line
+    return (
+      <div style={{color: "yellow", fontSize: 24}}>Style Me!</div>
+    );
+    // change code above this line
+  }
+};
